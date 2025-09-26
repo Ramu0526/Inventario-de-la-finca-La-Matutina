302573c8e643739ec6dc709a2510996250f38080
Ramu0526 <>
2025-09-26 15:57:56 -0500

    inventario ramu 3.9,46

--- inventario/migrations/0031_remove_ganado_parto.py
+++ inventario/migrations/0031_remove_ganado_parto.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='ganado',
-            name='parto',
-        ),
+        # migrations.RemoveField(
+        #     model_name='ganado',
+        #     name='parto',
+        # ),
     ]
